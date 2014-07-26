b39bde05b3e391483de2f97b37089171fa84d178
unknown <Ayooluwa>
2014-07-26 01:39:03 +0100

    ready to commit new changes for app.js object module

--- js/app.js
+++ js/app.js
@@ -1,125 +1,87 @@
+    /*************************
+        Google Map
+    **************************/
+
+            myApp = {
+
+                init: function() {
+                    myApp.config = {
+                        geocoder: new google.maps.Geocoder(),
+                        mapOptions: {
+                            zoom: 15,
+                            center: new google.maps.LatLng(6.3456, 80),
+                            mapTypeId: 'roadmap'
+                        },
+                        infowindow: new google.maps.InfoWindow(),
+                    };
+
+                    google.maps.event.addDomListener(window, 'load', myApp.geo);
+                    
+                },
+                geo: function(){
+                   myApp.config.map = new google.maps.Map(document.getElementById('map_canvas'), myApp.config.mapOptions);
+                },
+                codeLatLng: function(lat, lng){ 
+                    var latlng = new google.maps.LatLng(lat, lng);
+                    myApp.config.geocoder.geocode({'latLng': latlng}, function(results, status) {
+                        if (status == google.maps.GeocoderStatus.OK) {
+                            if (results[1]) {
+                                myApp.config.map.setZoom(15);
+                                myApp.config.marker = new google.maps.Marker({
+                                    position: latlng,
+                                    map: myApp.config.map
+                                });
+                                myApp.config.infowindow.setContent(results[1].formatted_address);
+                                myApp.config.infowindow.open(myApp.config.map, myApp.config.marker);
+                            } 
+                            else {
+                                alert('No results found');
+                            }
+                        } 
+                        else {
+                            alert('Geocoder failed due to: ' + status);
+                        }
+                    }); 
+                } 
+        };
+        $( document ).ready( myApp.init );
+    /*************************
+      Image Loaded by Default
+    **************************/
 (function (){
+    
+    var app = angular.module('flickr', []);
 
+    app.controller("flickrController", ["$http", "$scope", function( $http, $scope){
 
-	/*************************
-		Google Map
-	**************************/
+        var pics = this;
+        var findPix = 'Lagos, Nigeria';
+        
+        var config = {
+            params: {
+                api_key: "6bf279c5f9d9e36bea5b3fb83f7a44f6",
+                
+            }
+        };
+        $scope.search = function(){
+            $scope.picture = "";
+            $http({method: 'jsonp', url:'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6bf279c5f9d9e36bea5b3fb83f7a44f6&tags='+ $scope.picture +'&has_geo=1&extras=geo&format=json&jsoncallback=JSON_CALLBACK'})
+                .success(function(data, config){
+                    pics.results = data.photos.photo;
+                });
+        };
+        $scope.locate = function(val){
+            console.log(val);
+            console.log(val.latitude);
+            console.log(val.longitude);
+            myApp.codeLatLng(val.latitude, val.longitude)
+        };
+        $http({method: 'jsonp', url:'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6bf279c5f9d9e36bea5b3fb83f7a44f6&tags='+ findPix +'&has_geo=1&extras=geo&format=json&jsoncallback=JSON_CALLBACK'})
+            .success(function(data, config){
+                
+                 pics.results = data.photos.photo;
+                });
+            
+    }]);
 
-	var geocoder;
-	var map;
-	var infowindow = new google.maps.InfoWindow();
-	var marker;
-
-	function initialize() {
-	  geocoder = new google.maps.Geocoder();
-		  
-		  var latlng = new google.maps.LatLng(6.3456, 80);
-		  var mapOptions = {
-		    zoom: 15,
-		    center: latlng,
-		    mapTypeId: 'roadmap'
-		  }
-
-		  map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-		
-	}
-	
-	function codeLatLng(lat, lng) {
-		
-		  var latlng = new google.maps.LatLng(lat, lng);
-		
-		  geocoder.geocode({'latLng': latlng}, function(results, status) {
-		    if (status == google.maps.GeocoderStatus.OK) {
-		      if (results[1]) {
-		        map.setZoom(15);
-		        marker = new google.maps.Marker({
-		            position: latlng,
-		            map: map
-
-		        });
-
-		        infowindow.setContent(results[1].formatted_address);
-		        infowindow.open(map, marker);
-		      } 
-		      else {
-		        alert('No results found');
-		      }
-
-		    } 
-		    else {
-		      alert('Geocoder failed due to: ' + status);
-		    }
-
-		  });
-
-		}
-
-		google.maps.event.addDomListener(window, 'load', initialize);
-
-
-	/*************************
-	  Image Loaded by Default
-	**************************/
-	
-		var app = angular.module('flickr', []);
-		
-		app.controller("flickrController", ["$http", "$scope", function( $http, $scope){
-
-			var pics = this;
-			var findPix = 'Lagos, Nigeria';
-			 
-			var config = {
-	            params: {
-	                api_key: "6bf279c5f9d9e36bea5b3fb83f7a44f6",
-	                
-	            }
-       		 };
-
-       		 $scope.search = function(){
-       		 	
-       		 	$scope.picture = "";
-
-       		 	$http({method: 'jsonp', url:'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6bf279c5f9d9e36bea5b3fb83f7a44f6&tags='+ $scope.picture +'&has_geo=1&extras=geo&format=json&jsoncallback=JSON_CALLBACK'})
-				.success(function(data, config){
-					
-					 pics.results = data.photos.photo;
-					 
-
-					 console.log(data.photos.photo);
-					 console.log(data);
-
-				});
-       		 };
-
-       		 $scope.locate = function(val){
-       		 	console.log(val);
-       		 	console.log(val.latitude);
-       		 	console.log(val.longitude);
-
-       		 	codeLatLng(val.latitude, val.longitude)
-
-       		 };
-			
-			$http({method: 'jsonp', url:'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6bf279c5f9d9e36bea5b3fb83f7a44f6&tags='+ findPix +'&has_geo=1&extras=geo&format=json&jsoncallback=JSON_CALLBACK'})
-				.success(function(data, config){
-					
-					 pics.results = data.photos.photo;
-					 
-
-					 console.log(data.photos.photo);
-					 console.log(data);
-
-				});
-
-		
-
-		}]);
-
-		
 })();
-
-
-
-
-
-
